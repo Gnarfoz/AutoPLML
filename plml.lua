9b6f5196ca38bacba56ce8b995ba31473c0a8b9c
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2017-01-26 19:19:19 +0100

    set drycode to maximum

--- plml.lua
+++ plml.lua
@@ -1,6 +1,29 @@
 -- beware the loot gnomes
+local bosses = {}
+bosses["Trilliax"] = true
+bosses["Tichondrius"] = true
+bosses["Krosus"] = true
+bosses["Star Augur Etraeus"] = true
+bosses["Grand Magistrix Elisande"] = true
+bosses["Gul'dan"] = true
+
+local function check()
+	if not InCombatLockdown() then
+		local instance, type = IsInInstance()
+		if instance and type = "raid" then
+			for n=1,40 do
+				if UnitExists("raid"..n.."target") then
+					if bosses[UnitName("raid"..n.."target)] then
+						print("Interesting boss: " .. UnitName("raid"..n.."target"))
+					end
+				end
+			end
+		end
+	end
+end
+
 local function magic()
-	--stuff
+	local timer = C_Timer.NewTimer(5, check)
 end
 
 local function OnEvent(self, event, ...)
